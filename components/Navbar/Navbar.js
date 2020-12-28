87ec00c5cd5daf854cfef9f48cfc5d2ff5a69847
Eli Sultanov <elisultanov@Elis-MacBook-Pro.local>
2020-12-27 20:05:45 -0500

    fixed navbar on desktop

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import styles from './navbar.module.css'
 import { ImLinkedin2, ImGithub, ImTwitter } from 'react-icons/im'
 import { GoGrabber, GoX } from 'react-icons/go'
 import Image from 'next/image'
+import { isMobile } from 'react-device-detect'
 export default function Navbar() {
     const [toggle, setToggle] = useState(false);
     const handleClick = () => setToggle(!toggle);
@@ -16,7 +17,7 @@ export default function Navbar() {
                 className={styles.logo}
             />
             <span className={`${styles.toggle} ${styles.icons}`} onTouchEnd={handleClick}>{toggle ? <GoX /> : <GoGrabber />}</span>
-            <ul className={toggle ? `${styles.show}` : styles.menu}>
+            <ul className={toggle ? `${styles.menu} ${styles.active}` : styles.menu}>
                 <li className={`${styles.menu_icons} ${styles.icons}`} onClick={handleClick}> <a target='_blank' href='https://www.linkedin.com/in/elisvcodes/'><ImLinkedin2 /></a></li>
                 <li className={`${styles.menu_icons} ${styles.icons}`} onClick={handleClick}> <a target='_blank' href='https://github.com/elisvcodes'><ImGithub /></a></li>
                 <li className={`${styles.menu_icons} ${styles.icons}`} onClick={handleClick}> <a target='_blank' href='https://twitter.com/elisvcodes'><ImTwitter /></a></li>

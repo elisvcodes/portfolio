6120fe1d915143e3ddda82cff0cac143613605bf
Eli <69530035+elisvcodes@users.noreply.github.com>
2021-03-05 13:45:27 -0500

    updated date

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -4,7 +4,7 @@ import styles from './footer.module.css'
 export default function Footer() {
     return (
         <footer className={`section ${styles.footer}`}>
-            <p> &copy; 2020 Eli Sultanov</p>
+            <p> &copy; 2020-2021 Eli Sultanov</p>
         </footer>
     )
 }

f80974ac90fbc682ebd65beac9fbe5e05ecaa2f7
elisvcodes <sultanoveli@outlook.com>
2021-04-15 10:08:58 -0400

    Updated the footer dates

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import styles from './footer.module.css'
+import React from 'react';
+import styles from './footer.module.css';
 
 export default function Footer() {
-    return (
-        <footer className={`section ${styles.footer}`}>
-            <p> &copy; 2020 Eli Sultanov</p>
-        </footer>
-    )
+  return (
+    <footer className={`section ${styles.footer}`}>
+      <p> &copy; 2020-2021 Eli Sultanov</p>
+    </footer>
+  );
 }

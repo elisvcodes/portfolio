04996f47af20b97e984ea2c832af044d4e9186db
Eli <69530035+elisvcodes@users.noreply.github.com>
2021-04-15 10:14:29 -0400

    Update Footer.js

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -2,17 +2,9 @@ import React from 'react';
 import styles from './footer.module.css';
 
 export default function Footer() {
-<<<<<<< HEAD
   return (
     <footer className={`section ${styles.footer}`}>
       <p> &copy; 2020-2021 Eli Sultanov</p>
     </footer>
   );
-=======
-    return (
-        <footer className={`section ${styles.footer}`}>
-            <p> &copy; 2020-2021 Eli Sultanov</p>
-        </footer>
-    )
->>>>>>> 6120fe1d915143e3ddda82cff0cac143613605bf
 }

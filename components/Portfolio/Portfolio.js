529138f19a5be0074bbf54d9104103b6cc5a90df
elisvcodes <sultanoveli@outlook.com>
2021-07-22 10:39:26 -0400

    updated portfolio

--- components/Portfolio/Portfolio.js
+++ components/Portfolio/Portfolio.js
@@ -4,77 +4,17 @@ import Image from 'next/image';
 import { DiSafari, DiGithubAlt } from 'react-icons/di';
 export default function Portfolio() {
   return (
-    <div className="section">
+    <div className='section'>
       <h3 className={styles.portfolio_title}>Things I've built</h3>
       <div className={styles.portfolio}>
         <div className={`${styles.portfolio_box} ${styles.box1}`}>
-          <div className="portfolio_img">
+          <div className='portfolio_img'>
             <Image
-              src="/rsz_kore.png"
+              src='/twetwe.png'
               width={2660}
               height={1200}
-              objectPosition="fill"
-              objectFit="contain"
-            />
-          </div>
-          <h2 className={styles.portfolio_box_title}>KoreCSS</h2>
-          <p className={styles.portfolio_box_body}>
-            This was my first attempt at creating something for the open source
-            community. KoreCSS is a simple CSS library with core functions such
-            as a navbar, a grid system powered by flexbox, forms, and other util
-            classes that speed up the process of building websites.
-          </p>
-          <div className={styles.icons}>
-            <a href="https://github.com/elisvcodes/KoreCSS" target="_blank">
-              <DiGithubAlt />
-            </a>
-            <a href="https://elisvcodes.github.io/kore-site/" target="_blank">
-              <DiSafari />
-            </a>
-          </div>
-        </div>
-
-        <div className={`${styles.portfolio_box} ${styles.box2}`}>
-          <div className="portfolio_img">
-            <Image
-              src="/rsz_1bt.png"
-              width={2660}
-              height={1200}
-              objectPosition="fill"
-              objectFit="contain"
-            />
-          </div>
-          <h2 className={styles.portfolio_box_title}>Budget Tracker</h2>
-          <p className={styles.portfolio_box_body}>
-            The entire purpose of this project was to test my knowledge of
-            vanilla JavaScript and my understanding of the DOM. Once I was able
-            to keep track of different components and my code was not spaghetti
-            code, I moved on to learning libraries such as React.
-          </p>
-          <div className={styles.icons}>
-            <a
-              href="https://github.com/elisvcodes/budget-tracker/"
-              target="_blank"
-            >
-              <DiGithubAlt />
-            </a>
-            <a
-              href="https://elisvcodes.github.io/budget-tracker/"
-              target="_blank"
-            >
-              <DiSafari />
-            </a>
-          </div>
-        </div>
-
-        <div className={`${styles.portfolio_box} ${styles.box2}`}>
-          <div className="portfolio_img">
-            <Image
-              src="/twetwe.png"
-              width={2660}
-              height={1200}
-              objectPosition="fill"
-              objectFit="contain"
+              objectPosition='fill'
+              objectFit='contain'
             />
           </div>
           <h2 className={styles.portfolio_box_title}>Twe Twe</h2>
@@ -86,10 +26,98 @@ export default function Portfolio() {
             missing.
           </p>
           <div className={styles.icons}>
-            <a href="https://github.com/elisvcodes/TweTwe" target="_blank">
+            <a href='https://github.com/elisvcodes/TweTwe' target='_blank'>
               <DiGithubAlt />
             </a>
-            <a href="https://twetwe.elisv.com/" target="_blank">
+            <a href='https://twetwe.elisv.com/' target='_blank'>
+              <DiSafari />
+            </a>
+          </div>
+        </div>
+        <div className={`${styles.portfolio_box} ${styles.box2}`}>
+          <div className='portfolio_img'>
+            <Image
+              src='/offset-srn.png'
+              width={2660}
+              height={1200}
+              objectPosition='fill'
+              objectFit='contain'
+            />
+          </div>
+          <h2 className={styles.portfolio_box_title}>OffSet</h2>
+          <p className={styles.portfolio_box_body}>
+            OffSet is a platform that compares the carbon footprint between
+            reusable and non-reusable items. In the app, users can compare
+            between items and see graphically which item is more environmentally
+            sustainable. Users can save their favorite items by creating an
+            account.
+          </p>
+          <div className={styles.icons}>
+            <a href='https://github.com/elisvcodes/Offset' target='_blank'>
+              <DiGithubAlt />
+            </a>
+            <a href='https://offset.elisv.com/' target='_blank'>
+              <DiSafari />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className={styles.portfolio}>
+        <div className={`${styles.portfolio_box} ${styles.box1}`}>
+          <div className='portfolio_img'>
+            <Image
+              src='/rsz_kore.png'
+              width={2660}
+              height={1200}
+              objectPosition='fill'
+              objectFit='contain'
+            />
+          </div>
+          <h2 className={styles.portfolio_box_title}>KoreCSS</h2>
+          <p className={styles.portfolio_box_body}>
+            This was my first attempt at creating something for the open source
+            community. KoreCSS is a simple CSS library with core functions such
+            as a navbar, a grid system powered by flexbox, forms, and other util
+            classes that speed up the process of building websites.
+          </p>
+          <div className={styles.icons}>
+            <a href='https://github.com/elisvcodes/KoreCSS' target='_blank'>
+              <DiGithubAlt />
+            </a>
+            <a href='https://elisvcodes.github.io/kore-site/' target='_blank'>
+              <DiSafari />
+            </a>
+          </div>
+        </div>
+
+        <div className={`${styles.portfolio_box} ${styles.box2}`}>
+          <div className='portfolio_img'>
+            <Image
+              src='/rsz_1bt.png'
+              width={2660}
+              height={1200}
+              objectPosition='fill'
+              objectFit='contain'
+            />
+          </div>
+          <h2 className={styles.portfolio_box_title}>Budget Tracker</h2>
+          <p className={styles.portfolio_box_body}>
+            The entire purpose of this project was to test my knowledge of
+            vanilla JavaScript and my understanding of the DOM. Once I was able
+            to keep track of different components and my code was not spaghetti
+            code, I moved on to learning libraries such as React.
+          </p>
+          <div className={styles.icons}>
+            <a
+              href='https://github.com/elisvcodes/budget-tracker/'
+              target='_blank'
+            >
+              <DiGithubAlt />
+            </a>
+            <a
+              href='https://elisvcodes.github.io/budget-tracker/'
+              target='_blank'
+            >
               <DiSafari />
             </a>
           </div>
@@ -98,3 +126,5 @@ export default function Portfolio() {
     </div>
   );
 }
+
+//

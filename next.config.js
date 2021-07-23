b5299f2d800f4bf16edb8fc5082709b566ee2fa3
elisvcodes <sultanoveli@outlook.com>
2021-07-23 11:41:25 -0400

    create a webpack file for nextjs

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+      loader: 'file-loader',
+    });
+    return config;
+  },
+};
